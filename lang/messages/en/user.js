86e8c3dc24233605d56cf23a83ad5861dbea712d
lrpick8 <lpickrell1@my.bcit.ca>
2026-01-12 17:18:17 -0800

    added project structure and initial file copy of user.js in lang folders

---
+++ lang/messages/en/user.js
@@ -0,0 +1,7 @@
+export const APPSTRINGS = {
+    LABEL_PROMPT: "How many buttons to create?",
+    BTN_LABEL: "Go!",
+    ERROR_RANGE_MESSAGE: "Please enter a number between 3 and 7!",
+    MSG_SUCCESS: "Excellent Memory! You Win!",
+    MSG_FAILURE: "Wrong Order! Please try again!",
+};
